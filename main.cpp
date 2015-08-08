1a75261e7b789bef18568a3aacfc83720495f332
Alexander Platonov <gorodmehov@gmail.com>
2015-08-09 02:22:59 +0300

    Добавлен вывод лога

--- main.cpp
+++ main.cpp
@@ -16,14 +16,19 @@ using namespace coordinator;
  * 
  */
 int main(int argc, char** argv) {
+    cout<<"Hello!"<<endl;
     std::map <std::string, std::string> config;
     config.insert(std::pair<std::string, std::string>("port", "12345"));
     config.insert(std::pair<std::string, std::string>("clients", "12345"));
     config.insert(std::pair<std::string, std::string>("threadscount", "1"));
     config.insert(std::pair<std::string, std::string>("len_block", "1024"));
+    cout<<"Inited"<<endl;
     ::coordinator::coordinator * c = new ::coordinator::coordinator(config);
+    cout<<"Created"<<endl;
     c->play();
+    cout<<"Played"<<endl;
     cin.get();
+    cout<<"Bye!"<<endl;
     return 0;
 }
 

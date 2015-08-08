8fc8fab48dbb89ea4c3d26c699adb4b97abebe27
Alexander Platonov <gorodmehov@gmail.com>
2015-08-09 02:03:19 +0300

    Добавлена системная пауза до ножатия клавиши

--- main.cpp
+++ main.cpp
@@ -6,14 +6,24 @@
  */
 
 #include <cstdlib>
-
+#include <iostream>
+#include <string>
+#include "coordinator.h"
 using namespace std;
+using namespace coordinator;
 
 /*
  * 
  */
 int main(int argc, char** argv) {
-
+    std::map <std::string, std::string> config;
+    config.insert(std::pair<std::string, std::string>("port", "12345"));
+    config.insert(std::pair<std::string, std::string>("clients", "12345"));
+    config.insert(std::pair<std::string, std::string>("threadscount", "1"));
+    config.insert(std::pair<std::string, std::string>("len_block", "1024"));
+    ::coordinator::coordinator * c = new ::coordinator::coordinator(config);
+    c->play();
+    system("pause");
     return 0;
 }
 

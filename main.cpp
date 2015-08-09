0f6abe1e63494da306102367c525c8e1a8a3484b
Alexander Platonov <gorodmehov@gmail.com>
2015-08-09 03:33:46 +0300

    Теперь бесконечный цикл на ожидании

--- main.cpp
+++ main.cpp
@@ -16,19 +16,21 @@ using namespace coordinator;
  * 
  */
 int main(int argc, char** argv) {
-    cout<<"Hello!"<<endl;
+    cout << "Hello!" << endl;
     std::map <std::string, std::string> config;
     config.insert(std::pair<std::string, std::string>("port", "12345"));
     config.insert(std::pair<std::string, std::string>("clients", "12345"));
     config.insert(std::pair<std::string, std::string>("threadscount", "2"));
     config.insert(std::pair<std::string, std::string>("len_block", "1024"));
-    cout<<"Inited"<<endl;
+    cout << "Inited" << endl;
     ::coordinator::coordinator * c = new ::coordinator::coordinator(config);
-    cout<<"Created"<<endl;
+    cout << "Created" << endl;
     c->play();
-    cout<<"Played"<<endl;
-    cin.get();
-    cout<<"Bye!"<<endl;
+    cout << "Played" << endl;
+    while (1) {
+        cin.get();
+    }
+    cout << "Bye!" << endl;
     return 0;
 }
 
